bc6e918a2ce77cdc02c69e10b3632f5cb1428e64
alekhyajaddu <alekhyajaddu@gmail.com>
2020-02-17 16:18:27 -0600

    modified code

--- calc.js
+++ calc.js
@@ -1,17 +1,16 @@
 function calc()
 {
-    let Total=parseFloat(document.querySelector('#total-field').value)
-    let Rate=parseFloat(document.querySelector('#rate-field').value)
+    const Total=parseFloat(document.querySelector('#total-field').value)
+    const Rate=parseFloat(document.querySelector('#rate-field').value)
 
     console.log(Rate);
 
-    let Tip=Total*(Rate/100)
-    let TaxRate=5.5
-   // let Tax=Total*(TaxRate/100)
-    let GrandTotal=Total+Tip+TaxRate
+    const Tip=Total*(Rate/100)
+    const TaxRate=5.5
+    const Tax=Total*(TaxRate/100)
+    const GrandTotal=Total+Tip+Tax
 
     document.querySelector('#Tip').innerHTML= Tip
     document.querySelector('#Tax').innerHTML= TaxRate
     document.querySelector('#GrandTotal').innerHTML= GrandTotal
 }
-//document.querySelector('#calc-button').addEventListener('click',calc)
